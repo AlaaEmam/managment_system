8fe0e19d951b09746bf3054dcb3c33eca20f64a6
alaa emam <66056822+AlaaEmam@users.noreply.github.com>
2024-11-30 15:19:45 +0200

    design and logic Verification Page

--- src/modules/Auth/components/VerifyAccount/VerifyAccount.tsx
+++ src/modules/Auth/components/VerifyAccount/VerifyAccount.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Logo from '../../../../assets/logo.png';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';

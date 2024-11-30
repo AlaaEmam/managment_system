300c0e37fd2a7f303f39e46d7de71d71bb95077d
siegfried2020 <domydodo2015@gmail.com>
2024-11-30 18:07:47 +0200

    confict

--- src/modules/Shared/components/MasterLayout/MasterLayout.tsx
+++ src/modules/Shared/components/MasterLayout/MasterLayout.tsx
@@ -9,6 +9,7 @@ export default function MasterLayout({loginData}:MasterLayoutProps):any {
     <Navbar loginData={loginData}/>
     <div className='d-flex'>
       <div className="w-100">
+        
         <Outlet/>
       
       </div>

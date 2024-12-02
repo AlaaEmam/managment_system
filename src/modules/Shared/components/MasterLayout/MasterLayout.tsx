5a6cbc4562532860e24590dabf2572786ad81c83
alaa emam <66056822+AlaaEmam@users.noreply.github.com>
2024-12-02 23:43:07 +0200

    add navbar

--- src/modules/Shared/components/MasterLayout/MasterLayout.tsx
+++ src/modules/Shared/components/MasterLayout/MasterLayout.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import SideBar from '../SideBar/SideBar'
+import { Outlet } from 'react-router-dom'
+import Navbar from '../Navbar/Navbar'
+import Header from '../Header/Header'
 
 export default function MasterLayout() {
   return (
-<>
-<SideBar/>
+    <div className="d-flex flex-column">
+      <div className="w-100">
+        <Navbar/>
+      
+      </div>
 
-
-</>  )
+      <div className="d-flex border-0">
+        <SideBar/>
+        <div className="w-100">
+          <Header/>
+          <Outlet/>
+        </div>
+      </div>
+    
+  </div>  
+  )
 }

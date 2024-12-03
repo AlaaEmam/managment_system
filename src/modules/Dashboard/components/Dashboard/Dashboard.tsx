9d95367399bbe5a57669f3b5357ab7f882e028a8
ahmednasrcs <hamadnasrcs@gmail.com>
2024-12-03 22:24:45 +0200

    Resolved merge conflict

--- src/modules/Dashboard/components/Dashboard/Dashboard.tsx
+++ src/modules/Dashboard/components/Dashboard/Dashboard.tsx
@@ -3,9 +3,9 @@ import SideBar from '../../../Shared/components/SideBar/SideBar'
 import MasterLayout from '../../../Shared/components/MasterLayout/MasterLayout'
 
 export default function Dashboard() {
+
   return (
     <>
-    
     </>
   )
 }
--- src/modules/Shared/components/Header/Header.tsx
+++ src/modules/Shared/components/Header/Header.tsx
@@ -1,21 +1,22 @@
 import React from 'react'
 
 export default function Header() {
+
   return (
     <>
- <div className=" container-fluid">
-    <div className="row " >
-      <div className='p-4'>
-<div className= "header2  d-flex  align-items-center mb-4">
-      <div className="col-md-7 ps-5 text-white">
-      <h3 className="fw-bold h1 my-4">Welcome <span className="yellow ">user name</span></h3>
-      <span>You can add project and assign tasks to your team </span>
-    </div>
-    </div>
-      </div>
+      <div className=" container-fluid">
+        <div className="row " >
+          <div className='p-4'>
+            <div className="header2  d-flex  align-items-center mb-4">
+              <div className="col-md-7 ps-5 text-white">
+                <h3 className="fw-bold h1 my-4">Welcome <span className="yellow ">user name</span></h3>
+                <span>You can add project and assign tasks to your team </span>
+              </div>
+            </div>
+          </div>
 
-    </div>
-    </div>
+        </div>
+      </div>
     </>
   )
 }

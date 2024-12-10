ae22dec4c41abdb91b75d234c237556e4686bc7b
alaa emam <66056822+AlaaEmam@users.noreply.github.com>
2024-12-10 14:48:30 +0200

    SOLVE NAVBAR ERROR AND ADD LOGINDATA

--- src/modules/Shared/components/Navbar/Navbar.tsx
+++ src/modules/Shared/components/Navbar/Navbar.tsx
@@ -5,12 +5,14 @@ import Logo from '../../../../assets/logo-dark.png';
 import DefaultProfile from '../../../../assets/defaultavatar.jpg';
 
 import { AuthContext } from '../../../../context/AuthContext';
-
+import './Navbar_style.css';
 // I changed this.
 
 export default function Navbar() {
-  const {LoginData}:any=useContext(AuthContext);
-  console.log(LoginData?.userName)
+  
+  const { loginData } = useContext(AuthContext) || { loginData: null };
+    console.log('LoginData:', loginData);
+
   return (
     <>
 <div className='d-flex justify-content-between align-items-center navbar'>
@@ -29,10 +31,10 @@ export default function Navbar() {
       </span>
       </div>
     </div>
-    <img className="avatar-img mx-3" src={DefaultProfile} alt="User" />
+    <img className="avatar-img mx-3" src={loginData && loginData.imagePath ? loginData.imagePath : DefaultProfile} alt="User" />
     <div className='navbar-title'>
-      <h5 className='fw-light mb-0'>Upskilling</h5>
-      <p className='fw-lighter mt-2'>upskilling.eg1@gmail.com</p>
+      <h5 className='fw-light mb-0'>{loginData ? loginData.userName : 'Guest'}</h5>
+      <p className='fw-lighter mt-2'>{loginData ? loginData.userEmail : ''}</p>
     </div>
   </div>
 </div>

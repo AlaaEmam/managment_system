beb8eb6e854e7b6a49ae104023ec58ea818f3195
hanaahossien <hanaa.hoosien@gmail.com>
2024-12-10 23:53:20 +0200

    add dashBoard data & chart

--- src/modules/Dashboard/components/Dashboard/Dashboard.tsx
+++ src/modules/Dashboard/components/Dashboard/Dashboard.tsx
@@ -1,64 +1,70 @@
-import '../Dashboard.css';
-import Progress from '../../../../assets/icons/progress.svg';
-import Tasks from '../../../../assets/icons/task.svg';
-import Projects from '../../../../assets/icons/projects.svg';
-import DashboardBox from '../DashboardBox/DashboardBox';
-import Header from '../../../Shared/components/Header/Header';
-
+import "../Dashboard.css";
+import Progress from "../../../../assets/icons/progress.svg";
+import Tasks from "../../../../assets/icons/task.svg";
+import Projects from "../../../../assets/icons/projects.svg";
+import DashboardBox from "../DashboardBox/DashboardBox";
+import Header from "../../../Shared/components/Header/Header";
+import { PieChart } from "../PieChart/PieChart";
 
 export default function Dashboard() {
   const cardsData = [
     {
-        icon: Progress,
-        label: 'Progress',
-        value: '% 90',
-        iconBgClass: 'color-icon-bg-1'
-    },
-    {
-        icon: Tasks,
-        label: 'Tasks Number',
-        value: '1293',
-        iconBgClass: 'color-icon-bg-2'
-    },
-    {
-        icon: Projects,
-        label: 'Projects Number',
-        value: '32',
-        iconBgClass: 'color-icon-bg-3'
-    }
-];
-const cardsData2 = [
-  {
       icon: Progress,
-      label: 'Active',
-      value: '90',
-      iconBgClass: 'color-icon-bg-1'
-  },
-  {
+      label: "Progress",
+      value: "% 90",
+      iconBgClass: "color-icon-bg-1",
+    },
+    {
       icon: Tasks,
-      label: 'Inactive',
-      value: '13',
-      iconBgClass: 'color-icon-bg-2'
-  },
+      label: "Tasks Number",
+      value: "1293",
+      iconBgClass: "color-icon-bg-2",
+    },
+    {
+      icon: Projects,
+      label: "Projects Number",
+      value: "32",
+      iconBgClass: "color-icon-bg-3",
+    },
+  ];
+  const cardsData2 = [
+    {
+      icon: Progress,
+      label: "Active",
+      value: "90",
+      iconBgClass: "color-icon-bg-1",
+    },
+    {
+      icon: Tasks,
+      label: "Inactive",
+      value: "13",
+      iconBgClass: "color-icon-bg-2",
+    },
+  ];
 
-];
+
+
+
   return (
-      <>
-    <Header/>
-    <div className='container'>
-      <div className='row d-flex justify-content-around g-4 mx-3'>
-        <DashboardBox 
-            title="Tasks" 
-            description="Lorem ipsum dolor sit amet, consectetur." 
-            cards={cardsData} 
-        />
-        <DashboardBox 
-            title="User" 
-            description="More details can be added here." 
-            cards={cardsData2} 
-        />
-      </div>
-  </div>
-  </>
+    <>
+      <Header />
+      {/* <div className="container-fluid">
+        <div className="row d-flex justify-content-around g-4 mx-3">
+          <DashboardBox
+            title="Tasks"
+            description="Lorem ipsum dolor sit amet, consectetur."
+            cards={cardsData}
+          />
+          <DashboardBox
+            title="User"
+            description="More details can be added here."
+            cards={cardsData2}
+          />
+        </div>
+      </div> */}
+
+        <div className="">
+      <PieChart/></div>
+    </>
   );
 }
--- src/modules/Shared/components/MasterLayout/MasterLayout.tsx
+++ src/modules/Shared/components/MasterLayout/MasterLayout.tsx
@@ -1,23 +1,19 @@
 import React from "react";
 import SideBar from "../SideBar/SideBar";
 import { Outlet } from "react-router-dom";
-import Navbar from '../Navbar/Navbar'
+import Navbar from "../Navbar/Navbar";
 
 export default function MasterLayout() {
   return (
     <>
-
-
-        <div className=" d-flex">
-
+      <div className=" d-flex">
         <div className="position-sticky top-0 vh-100 ">
-            <SideBar />
-          </div>
-          <div className="w-100">
-          <Outlet />
-
-          </div>
+          <SideBar />
         </div>
+        <div className="w-100">
+          <Outlet />
+        </div>
+      </div>
     </>
   );
 }
